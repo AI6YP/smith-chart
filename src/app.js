d59d02ac0d520eed0091699266cb7a01c2a42aa7
drom <alex.drom@gmail.com>
2018-02-28 23:52:52 -0800

    eslint fix

--- src/app.js
+++ src/app.js
@@ -22,32 +22,32 @@ const Demo1 = (config) => {
 
     return (
         $('svg', {width: size, height: size},
-          $('defs', {},
-            $('style', {}, `
+            $('defs', {},
+                $('style', {}, `
 .l1 { stroke: black; fill: none; stroke-linecap: round; stroke-width: 3 }
 .l2 { stroke: black; fill: none; stroke-linecap: round; }
 .trace { stroke: #cd0000; fill: none; stroke-linecap: round; stroke-width: 5 }
 `
+                )
+            ),
+            $('rect', {
+                x: 0, y: 0,
+                width: size, height: size,
+                fill: '#fffad9',
+                rx: 8
+            }),
+            $('g', {transform: `translate(${ size - margin.left },${ size / 2 })`},
+                $(SC.Grid.Re, {r: r, lines: SC.grid }),
+                $(SC.Grid.Im, {r: r, lines: SC.grid }),
+                $(SC.ReLabels, { r: r, lines: SC.reLabels0 }),
+                $(SC.Shape, {
+                    r: r,
+                    data: data,
+                    className: 'trace',
+                    x: e => e[0],
+                    y: e => e[1]
+                })
             )
-          ),
-          $('rect', {
-            x: 0, y: 0,
-            width: size, height: size,
-            fill: '#fffad9',
-            rx: 8
-          }),
-          $('g', {transform: `translate(${ size - margin.left },${ size / 2 })`},
-            $(SC.Grid.Re, {r: r, lines: SC.grid }),
-            $(SC.Grid.Im, {r: r, lines: SC.grid }),
-            $(SC.ReLabels, { r: r, lines: SC.reLabels0 }),
-            $(SC.Shape, {
-              r: r,
-              data: data,
-              className: 'trace',
-              x: e => e[0],
-              y: e => e[1]
-            })
-          )
         )
     );
 };
